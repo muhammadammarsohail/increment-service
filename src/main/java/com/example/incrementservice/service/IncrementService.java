7f234699f35e46df3c4e92b276ab98623e1b38e2
Ammar Sohail <ammarsohail@Ammars-MacBook-Pro.local>
2023-02-13 17:08:46 +0500

    implement structured logging

--- src/main/java/com/example/incrementservice/service/IncrementService.java
+++ src/main/java/com/example/incrementservice/service/IncrementService.java
@@ -1,7 +1,19 @@
 package com.example.incrementservice.service;
+//import java.util.logging.Logger;
 
+
+//import lombok.extern.slf4j.Slf4j;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Slf4j
 public class IncrementService {
+//    private static final Logger log = LoggerFactory.getLogger(IncrementService.class);
+//    private static final Logger log = Logger.getLogger(IncrementService.class.getName());
     public int add(int first, int second) {
+        log.info("Welcome to the log");
         return first + second;
     }
 }
